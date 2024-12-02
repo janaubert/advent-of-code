809b7c47142bc7b61c89141d45ac1420cd10a844
Jan <janaubert92@gmail.com>
2024-12-02 18:18:05 +0100

    test

---
+++ 2024/day_two.py
@@ -0,0 +1,14 @@
+with open('../ADVENT OF CODE/2024/input/day_one.txt') as file:
+    f = [x.strip().split(" ") for x in file]
+    
+
+def part_one():
+    pass
+    
+
+def part_two():
+    pass
+
+
+print(part_one())
+print(part_two())
